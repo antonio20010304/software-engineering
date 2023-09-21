4e534112508f94b804bb4f0c7faac10969c1e602
antonio20010304 <126451277+antonio20010304@users.noreply.github.com>
2023-09-21 15:48:34 +0300

    commit folder with all changes, final version
    Пупупууууу надо что-то делать....

---
+++ Main/Main/EmptyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace name;
+
+public abstract class Program
+{
+	public static void Main(string[] args)
+	{
+		Helicopter hel = new Helicopter();
+		Console.WriteLine(hel);
+	}
+	
+}
+
